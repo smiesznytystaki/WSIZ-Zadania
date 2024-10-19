55b1038f96c9a54f093b5048d1f39e4ca6f63567
szef <kryst.03@o2.pl>
2024-10-19 21:49:19 +0200

    Zad5

---
+++ Zad5.py
@@ -0,0 +1,8 @@
+a = float(input("Podaj długość boku a: "))
+b = float(input("Podaj długość boku b: "))
+
+pole = a*b
+obwod = 2*a+2*b
+
+print("Pole prostokąta wynosi: "+str(pole))
+print("Obwód prostokąta wynosi: "+str(obwod))
